52662a73f514fb2fe23ad1c4c3c432bf8dd6baae
Kri4evskiy <kri4evskiy@gmail.com>
2020-06-19 17:33:03 +0300

    Profile, 404, heroku?

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const csrf = require('csurf')
 const flash = require('connect-flash')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
+const compression = require('compression')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const MongoStore = require('connect-mongodb-session')(session)
@@ -12,8 +14,11 @@ const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
+const profileRoutes = require('./routes/profile')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const fileMiddleware = require('./middleware/file')
+const errorHandler = require('./middleware/error')
 const keys = require('./keys')
 
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
@@ -37,6 +42,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images',express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
   secret: keys.SESSION_SECRET,
@@ -44,8 +50,11 @@ app.use(session({
   saveUninitialized: false,
   store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
+app.use(helmet())
+app.use(compression())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
@@ -55,6 +64,9 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 
--- ExpressJS/keys/index.js
+++ ExpressJS/keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://stanislav:BCdF5wZPvYPIkTcm@cluster0-4s4z3.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'kri4evskiy@gmail.com',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
