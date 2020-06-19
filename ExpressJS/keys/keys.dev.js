01288f415c75e09e0a3cca48540642fceac8d38b
Kri4evskiy <kri4evskiy@gmail.com>
2020-06-19 17:46:43 +0300

    ...

---
+++ ExpressJS/keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://stanislav:BCdF5wZPvYPIkTcm@cluster0-4s4z3.mongodb.net/shop',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'kri4evskiy@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
