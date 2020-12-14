2847f8028ddb340bb9f638148d81d434fb100d6b
Kri4evskiy <kri4evskiy@gmail.com>
2020-12-14 19:06:56 +0200

    New proj init commit. TypeScript, Material-UI

---
+++ Twitter-clone/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button>
+      Hello World
+    </Button>
+    </div>
+  )
+}
+
+export default App
